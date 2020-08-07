Fix normalization and add better logging

examples/stallion.py
@@ -30,10 +30,12 @@ data["log_volume"] = np.log(data.volume + 1e-8)
 
 data["time_idx"] = data["date"].dt.year * 12 + data["date"].dt.month
 data["time_idx"] -= data["time_idx"].min()
+data["avg_volume_by_sku"] = data.groupby(["time_idx", "sku"], observed=True).volume.transform("mean")
+data["avg_volume_by_agency"] = data.groupby(["time_idx", "agency"], observed=True).volume.transform("mean")
 # data = data[lambda x: (x.sku == data.iloc[0]["sku"]) & (x.agency == data.iloc[0]["agency"])]
 
 training_cutoff = data["time_idx"].max() - 6
-max_encoder_length = 32
+max_encoder_length = 12
 max_prediction_length = 6
 
 training = TimeSeriesDataSet(
@@ -41,8 +43,9 @@ training = TimeSeriesDataSet(
     time_idx="time_idx",
     target="volume",
     group_ids=["agency", "sku"],
-    # min_encoder_length=max_encoder_length,
+    min_encoder_length=max_encoder_length,
     max_encoder_length=max_encoder_length,
+    min_prediction_length=max_prediction_length,
     max_prediction_length=max_prediction_length,
     static_categoricals=["agency", "sku"],
     static_reals=["avg_population_2017", "avg_yearly_household_income_2017"],
@@ -63,7 +66,15 @@ training = TimeSeriesDataSet(
     ],
     time_varying_known_reals=["time_idx", "price_regular", "discount_in_percent"],
     time_varying_unknown_categoricals=[],
-    time_varying_unknown_reals=["volume", "log_volume", "industry_volume", "soda_volume", "avg_max_temp"],
+    time_varying_unknown_reals=[
+        "volume",
+        "log_volume",
+        "industry_volume",
+        "soda_volume",
+        "avg_max_temp",
+        "avg_volume_by_sku",
+        "avg_volume_by_agency",
+    ],
     target_normalizer=GroupNormalizer(groups=["agency", "sku"], coerce_positive=True),
 )
 
@@ -72,6 +83,9 @@ batch_size = 128
 train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=0)
 val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size, num_workers=0)
 
+# save datasets
+training.save("training.pkl")
+validation.save("validation.pkl")
 
 early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=15, verbose=False, mode="min")
 lr_logger = LearningRateLogger()
@@ -97,11 +111,12 @@ tft = TemporalFusionTransformer.from_dataset(
     learning_rate=0.01,
     hidden_size=32,
     attention_head_size=2,
-    dropout=0.15,
-    hidden_continuous_size=32,
+    dropout=0.1,
+    hidden_continuous_size=16,
     output_size=7,
     loss=QuantileLoss(),
     log_interval=2,
+    log_val_interval=0.2,
     reduce_on_plateau_patience=3,
 )
 print(f"Number of parameters in network: {tft.size()/1e3:.1f}k")

pytorch_forecasting/data.py
@@ -3,6 +3,7 @@ Timeseries data is special and has to be processed and fed to algorithms in a sp
 defines a class that is able to handle a wide variety of timeseries data problems.
 """
 
+import pickle
 import warnings
 from copy import deepcopy
 import inspect
@@ -124,7 +125,7 @@ class GroupNormalizer(BaseEstimator, TransformerMixin):
 
     def _preprocess_y(self, y):
         if self.coerce_positive is None and not self.log_scale:
-            self.coerce_positive = [False, "relu"](y >= 0).all()
+            self.coerce_positive = (y >= 0).all()
 
         if self.log_scale:
             y = np.log(y + self.log_zero_value)
@@ -201,7 +202,10 @@ class GroupNormalizer(BaseEstimator, TransformerMixin):
     def transform(self, y: pd.Series, X: pd.DataFrame, return_norm: bool = False) -> pd.DataFrame:
         norm = self.get_norm(X)
         y = self._preprocess_y(y)
-        y_normed = (y / (norm[:, 0] + self.eps) - 1) / (norm[:, 1] + self.eps)
+        if self.center:
+            y_normed = (y / (norm[:, 0] + self.eps) - 1) / (norm[:, 1] + self.eps)
+        else:
+            y_normed = y / (norm[:, 0] + self.eps)
         if return_norm:
             return y_normed, norm
         else:
@@ -256,7 +260,10 @@ class GroupNormalizer(BaseEstimator, TransformerMixin):
         norm = data["target_scale"]
         if data["prediction"].ndim > 1:
             norm = norm.unsqueeze(-1)
-        y_normed = (data["prediction"] * norm[:, 1, None] + 1) * norm[:, 0, None]
+        if self.center:
+            y_normed = (data["prediction"] * norm[:, 1, None] + 1) * norm[:, 0, None]
+        else:
+            y_normed = data["prediction"] * norm[:, 0, None]
         if self.log_scale:
             y_normed = (y_normed.exp() - self.log_zero_value).clamp_min(0.0)
         elif isinstance(self.coerce_positive, bool) and self.coerce_positive:
@@ -394,7 +401,8 @@ class TimeSeriesDataSet(Dataset):
                 "relative_time_idx" not in data.columns
             ), "relative_time_idx is a protected column and must not be present in data"
             if "relative_time_idx" not in self.time_varying_known_reals:
-                self.time_varying_known_reals.append("relative_time_idx")
+                if "relative_time_idx" not in self.reals:
+                    self.time_varying_known_reals.append("relative_time_idx")
             data["relative_time_idx"] = 0.0  # dummy - real value will be set dynamiclly in __getitem__()
 
         # preprocess data
@@ -405,6 +413,32 @@ class TimeSeriesDataSet(Dataset):
 
         # convert to torch tensor for high performance data loading later
         self.data = self._data_to_tensors(data)
+
+    def save(self, fname: str) -> None:
+        """
+        Save dataset to disk
+
+        Args:
+            fname (str): filename to save to
+        """
+        with open(fname, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load(cls, fname: str):
+        """
+        Load dataset from disk
+
+        Args:
+            fname (str): filename to load from
+
+        Returns:
+            TimeSeriesDataSet
+        """
+        with open(fname, "rb") as file:
+            obj = pickle.load(file)
+        assert isinstance(obj, cls), f"Loaded file is not of class {cls}"
+        return obj
 
     def _preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
 
@@ -441,16 +475,20 @@ class TimeSeriesDataSet(Dataset):
                         feature_name not in data.columns
                     ), f"{feature_name} is a protected column and must not be present in data"
                     data[feature_name] = scales[:, idx].squeeze()
-                    self.static_reals.append(feature_name)
+                    if feature_name not in self.reals:
+                        self.static_reals.append(feature_name)
             else:
                 data[self.target] = self.target_normalizer.transform(data[self.target], data)
 
-        # rescale continuous variables
+        # rescale continuous variables apart from target
         for name in self.reals:
             if name not in self.scalers:
-                self.scalers[name] = StandardScaler().fit(data[[name]])
-            if self.scalers[name] is not None:
-                if isinstance(self.scales[name], GroupNormalizer):
+                if name != self.target:
+                    self.scalers[name] = StandardScaler().fit(data[[name]])
+                else:
+                    self.scalers[name] = self.target_normalizer
+            if self.scalers[name] is not None and name != self.target:
+                if isinstance(self.scalers[name], GroupNormalizer):
                     data[name] = self.scalers[name].transform(data[name], data)
                 else:
                     data[name] = self.scalers[name].transform(data[[name]]).reshape(-1)
@@ -885,6 +923,7 @@ class TimeSeriesDataSet(Dataset):
                 mean = scaler.mean_[0]
                 scale = scaler.scale_[0]
             else:
-                raise ValueError(f"Scales extraction for scaler of type {type(scaler)} not implemented")
+                mean = np.nan
+                scale = np.nan
             scales[name] = (mean, scale)
         return scales

pytorch_forecasting/metrics.py
@@ -20,10 +20,9 @@ class Metric(TensorMetric, metaclass=abc.ABCMeta):
     Other metrics should inherit from this base class
     """
 
-    def __init__(
-        self, name: str, quantiles: List[float] = [0.5],
-    ):
+    def __init__(self, name: str, quantiles: List[float] = [0.5], reduction="mean"):
         self.quantiles = quantiles
+        self.reduction = "mean"
         super().__init__(name)
 
     @abstractmethod
@@ -129,16 +128,18 @@ class MultiHorizonMetric(Metric):
         mask = torch.arange(target.size(1), device=target.device).unsqueeze(0) >= lengths.unsqueeze(-1)
         if losses.ndim > 2:
             mask = mask.unsqueeze(-1)
-        losses = losses.masked_fill(mask, 0.0)
-
         # reduce to one number
-        loss = losses.sum() / lengths.sum()
-        assert not torch.isnan(
-            loss
-        ), "Loss should not be nan - i.e. something went wrong in calculating the loss (e.g. log of a negative number)"
-        assert torch.isfinite(
-            loss
-        ), "Loss should not be infinite - i.e. something went wrong (e.g. input is not in log space)"
+        if self.reduction == "mean":
+            losses = losses.masked_fill(mask, 0.0)
+            loss = losses.sum() / lengths.sum()
+            assert not torch.isnan(
+                loss
+            ), "Loss should not be nan - i.e. something went wrong in calculating the loss (e.g. log of a negative number)"
+            assert torch.isfinite(
+                loss
+            ), "Loss should not be infinite - i.e. something went wrong (e.g. input is not in log space)"
+        elif self.reduction == "none":
+            loss = losses.masked_fill(mask, "nan")
         return loss
 
 

pytorch_forecasting/models/base_model.py
@@ -61,8 +61,8 @@ class BaseModel(LightningModule):
 
     def __init__(
         self,
-        log_interval=-1,
-        log_val_interval: int = None,
+        log_interval: Union[int, float] = -1,
+        log_val_interval: Union[int, float] = None,
         learning_rate: Union[float, List[float]] = 1e-3,
         log_gradient_flow: bool = False,
         loss: TensorMetric = SMAPE(),
@@ -76,8 +76,9 @@ class BaseModel(LightningModule):
         BaseModel for timeseries forecasting from which to inherit from
 
         Args:
-            log_interval (int, optional): batches after which predictions are logged. Defaults to -1.
-            log_val_interval (int, optional): batches after which predictions for validation are logged. Defaults to 
+            log_interval (Union[int, float], optional): Batches after which predictions are logged. If < 1.0, will log 
+                multiple entries per batch. Defaults to -1.
+            log_val_interval (Union[int, float], optional): batches after which predictions for validation are logged. Defaults to 
                 None/log_interval.
             learning_rate (float, optional): Learning rate. Defaults to 1e-3.
             log_gradient_flow (bool): If to log gradient flow, this takes time and should be only done to diagnose 
@@ -92,7 +93,8 @@ class BaseModel(LightningModule):
                 position (e.g. ``"0"`` for first position) to constraint (``-1`` for negative and ``+1`` for positive, 
                 larger numbers add more weight to the constraint vs. the loss but are usually not necessary). 
                 This constraint significantly slows down training. Defaults to {}.
-            output_transformer (Callable): transformer that takes network output and transforms it to prediction space. Defaults to None which is equivalent to ``lambda out: out["prediction"]``.
+            output_transformer (Callable): transformer that takes network output and transforms it to prediction space. 
+                Defaults to None which is equivalent to ``lambda out: out["prediction"]``.
         """
         super().__init__()
         # update hparams
@@ -226,38 +228,45 @@ class BaseModel(LightningModule):
 
     def _log_prediction(self, x, y_pred, batch_idx, label="train"):
         # log single prediction figure
-        if batch_idx % self.log_interval(label == "train") == 0 and self.log_interval(label == "train") > 0:
-            y_all = torch.cat(
-                [x["encoder_target"][0], x["decoder_target"][0]]
-            )  # all true values for y of the first sample in batch
-            if y_all.ndim == 2:  # timesteps, (target, weight), i.e. weight is included
-                y_all = y_all[:, 0]
-            max_encoder_length = x["encoder_lengths"].max()
-            fig = self.plot_prediction(
-                torch.cat(
-                    (
-                        y_all[: x["encoder_lengths"][0]],
-                        y_all[max_encoder_length : (max_encoder_length + x["decoder_lengths"][0])],
-                    ),
-                ),
-                y_pred[0, : x["decoder_lengths"][0]].detach(),
-            )  # first in batch
-            tag = f"{label.capitalize()} prediction"
-            if label == "train":
-                tag += f" of item 0 in global batch {self.global_step}"
+        log_interval = self.log_interval(label == "train")
+        if (batch_idx % log_interval == 0 or log_interval < 1.0) and log_interval > 0:
+            if log_interval < 1.0:  # log multiple steps
+                log_indices = torch.arange(0, len(y_pred), round(log_interval * len(y_pred)))
             else:
-                tag += f" of item 0 in batch {batch_idx}"
-            self.logger.experiment.add_figure(
-                tag, fig, global_step=self.global_step,
-            )
+                log_indices = [0]
 
-    def plot_prediction(self, y: torch.Tensor, y_hat: torch.Tensor) -> plt.Figure:
+            for idx in log_indices:
+                # all true values for y of the first sample in batch
+                y_all = torch.cat([x["encoder_target"][idx], x["decoder_target"][idx]])
+                if y_all.ndim == 2:  # timesteps, (target, weight), i.e. weight is included
+                    y_all = y_all[:, 0]
+                max_encoder_length = x["encoder_lengths"].max()
+                fig = self.plot_prediction(
+                    torch.cat(
+                        (
+                            y_all[: x["encoder_lengths"][0]],
+                            y_all[max_encoder_length : (max_encoder_length + x["decoder_lengths"][0])],
+                        ),
+                    ),
+                    y_pred[idx, : x["decoder_lengths"][0]].detach(),
+                )  # first in batch
+                tag = f"{label.capitalize()} prediction"
+                if label == "train":
+                    tag += f" of item {idx} in global batch {self.global_step}"
+                else:
+                    tag += f" of item {idx} in batch {batch_idx}"
+                self.logger.experiment.add_figure(
+                    tag, fig, global_step=self.global_step,
+                )
+
+    def plot_prediction(self, y: torch.Tensor, y_hat: torch.Tensor, add_loss_to_title: bool = True) -> plt.Figure:
         """
         Plot prediction of prediction vs actuals
 
         Args:
             y: all actual values
             y_hat: predictions
+            add_loss_to_title: if to add loss to title. Default to True.
 
         Returns:
             matplotlib figure
@@ -299,8 +308,8 @@ class BaseModel(LightningModule):
                 ax.errorbar(
                     x_pred, torch.tensor([[y_quantiles[0, i]], [y_quantiles[0, -i - 1]]]), c=pred_color, capsize=1.0,
                 )
-        loss = self.loss(y_hat[None], y[-n_pred:][None])
-        ax.set_title(f"Loss {loss:.3g}")
+        if add_loss_to_title:
+            ax.set_title(f"Loss {self.loss(y_hat[None], y[-n_pred:][None]):.3g}")
         ax.set_xlabel("Time index")
         fig.legend()
         return fig
@@ -443,7 +452,7 @@ class BaseModel(LightningModule):
             show_progress_bar: if to show progress bar. Defaults to True
 
         Returns:
-            output, index, decoder_lengths: some elements might not be present depending on what is configured 
+            output, index, decoder_lengths: some elements might not be present depending on what is configured
                 to be returned
         """
         # convert to dataloader
@@ -484,7 +493,7 @@ class BaseModel(LightningModule):
                     # mask non-predictions
                     out = out.masked_fill(nan_mask, torch.tensor(float("nan")))
                 elif mode == "quantiles":
-                    out = self.loss.to_quantiles(out)
+                    out = self.loss.to_quantiles(out["prediction"])
                     # mask non-predictions
                     out = out.masked_fill(nan_mask.unsqueeze(-1), torch.tensor(float("nan")))
                 elif mode == "raw":
@@ -501,7 +510,10 @@ class BaseModel(LightningModule):
         if isinstance(mode, (tuple, list)) or mode != "raw":
             output = torch.cat(output, dim=0)
         elif mode == "raw":
-            output = {name: torch.cat(values, dim=0) for name, values in output.items()}
+            output_cat = {}
+            for name in output[0].keys():
+                output_cat[name] = torch.cat([out[name] for out in output], dim=0)
+            output = output_cat
 
         if return_decoder_lengths:
             decoder_lengths = torch.cat(decode_lenghts, dim=0)

pytorch_forecasting/models/baseline.py
@@ -20,7 +20,8 @@ class Baseline(BaseModel):
         max_prediction_length = x["decoder_lengths"].max()
         assert x["encoder_lengths"].min() > 0, "Encoder lengths of at least 1 required to obtain last value"
         last_values = x["encoder_target"][torch.arange(x["encoder_target"].size(0)), x["encoder_lengths"] - 1]
-        return last_values[:, None, None].expand(-1, max_prediction_length, self.hparam.output_size)
+        prediction = last_values[:, None, None].expand(-1, max_prediction_length, self.hparams.output_size)
+        return dict(prediction=prediction)
 
     def _step(self, batch, batch_idx, label="train"):
         """

pytorch_forecasting/models/temporal_fusion_transformer/__init__.py
@@ -51,8 +51,8 @@ class TemporalFusionTransformer(BaseModel):
         categorical_labels: Dict[str, str] = {},
         real_scales: Dict[str, Tuple[float, float]] = {},
         learning_rate: float = 1e-3,
-        log_interval: int = 10,
-        log_val_interval: int = None,
+        log_interval: Union[int, float] = -1,
+        log_val_interval: Union[int, float] = None,
         log_gradient_flow: bool = False,
         reduce_on_plateau_patience: int = 1000,
         monotone_constaints: Dict[str, int] = {},
@@ -90,7 +90,8 @@ class TemporalFusionTransformer(BaseModel):
                 transform them into their original shape
             categorical_labels: dictionary mapping (string) indices to categorical variable names
             learning_rate: learning rate
-            log_interval: log predictions every x batches, do not log if 0 or less, log interpretation if > 0
+            log_interval: log predictions every x batches, do not log if 0 or less, log interpretation if > 0. If < 1.0
+                , will log multiple entries per batch. Defaults to -1.
             log_val_interval: frequency with which to log validation set metrics, defaults to log_interval
             log_gradient_flow: if to log gradient flow, this takes time and should be only done to diagnose training
                 failures
@@ -241,13 +242,6 @@ class TemporalFusionTransformer(BaseModel):
         self.static_context_enrichment = GatedResidualNetwork(
             self.hparams.hidden_size, self.hparams.hidden_size, self.hparams.hidden_size, self.hparams.dropout
         )
-        self.static_enrichment = GatedResidualNetwork(
-            input_size=self.hparams.hidden_size,
-            hidden_size=self.hparams.hidden_size,
-            output_size=self.hparams.hidden_size,
-            dropout=self.hparams.dropout,
-            context_size=self.hparams.hidden_size,
-        )
 
         # lstm encoder (history) and decoder (future) for local processing
         self.lstm_encoder = nn.LSTM(
@@ -271,6 +265,15 @@ class TemporalFusionTransformer(BaseModel):
         self.post_lstm_gate_decoder = GLU(self.hparams.hidden_size, dropout=self.hparams.dropout)
         self.post_lstm_add_norm_encoder = AddNorm(self.hparams.hidden_size)
         self.post_lstm_add_norm_decoder = AddNorm(self.hparams.hidden_size)
+
+        # static enrichment and processing past LSTM
+        self.static_enrichment = GatedResidualNetwork(
+            input_size=self.hparams.hidden_size,
+            hidden_size=self.hparams.hidden_size,
+            output_size=self.hparams.hidden_size,
+            dropout=self.hparams.dropout,
+            context_size=self.hparams.hidden_size,
+        )
 
         # attention for long-range processing
         self.multihead_attn = InterpretableMultiHeadAttention(
@@ -428,8 +431,14 @@ class TemporalFusionTransformer(BaseModel):
         # indices to which is attended
         attend_step = torch.arange(decoder_length, device=self.device)
         # indices for which is predicted
-        predict_step = torch.arange(decoder_length, 0, step=-1, device=self.device)[:, None]
-        # do not attend to steps after to prediction
+        predict_step = torch.arange(0, decoder_length, device=self.device)[:, None]
+        # do not attend to steps to self or after prediction
+        # todo: there is potential value in attending to future forecasts if they are made with knowledge currently
+        #   available
+        #   one possibility is here to use a second attention layer for future attention (assuming different effects
+        #   matter in the future than the past)
+        #   or alternatively using the same layer but allowing forward attention - i.e. only masking out non-available
+        #   data and self
         decoder_mask = attend_step >= predict_step
         # do not attend to steps where data is padded
         encoder_mask = self._get_mask(encoder_lengths.max(), encoder_lengths)

pytorch_forecasting/models/temporal_fusion_transformer/sub_modules.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 
 class TimeDistributedInterpolation(nn.Module):
-    def __init__(self, output_size, batch_first=False, trainable=False):
+    def __init__(self, output_size: int, batch_first: bool = False, trainable: bool = False):
         super().__init__()
         self.output_size = output_size
         self.batch_first = batch_first
@@ -20,7 +20,7 @@ class TimeDistributedInterpolation(nn.Module):
     def interpolate(self, x):
         upsampled = F.interpolate(x.unsqueeze(1), self.output_size, mode="linear", align_corners=True).squeeze(1)
         if self.trainable:
-            upsampled = upsampled * self.gate(self.mask.unsqueeze(0))
+            upsampled = upsampled * self.gate(self.mask.unsqueeze(0)) * 2.0
         return upsampled
 
     def forward(self, x):
@@ -45,7 +45,7 @@ class TimeDistributedInterpolation(nn.Module):
 class GLU(nn.Module):
     """Gated Linear Unit"""
 
-    def __init__(self, input_size, hidden_size=None, dropout=None):
+    def __init__(self, input_size: int, hidden_size: int = None, dropout: float = None):
         super().__init__()
 
         if dropout is not None:
@@ -66,7 +66,7 @@ class GLU(nn.Module):
 
 
 class AddNorm(nn.Module):
-    def __init__(self, input_size=None, trainable_add: bool = True):
+    def __init__(self, input_size: int = None, trainable_add: bool = True):
         super().__init__()
 
         self.input_size = input_size
@@ -79,13 +79,13 @@ class AddNorm(nn.Module):
 
     def forward(self, x, skip):
         if self.trainable_add:
-            skip = skip * self.gate(self.mask)
+            skip = skip * self.gate(self.mask) * 2.0
         output = self.norm(x + skip)
         return output
 
 
 class GateAddNorm(nn.Module):
-    def __init__(self, input_size, hidden_size=None, trainable_add: bool = True, dropout: float = 0.1):
+    def __init__(self, input_size: int, hidden_size: int = None, trainable_add: bool = True, dropout: float = 0.1):
         super().__init__()
 
         self.input_size = input_size
@@ -102,7 +102,15 @@ class GateAddNorm(nn.Module):
 
 
 class GatedResidualNetwork(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, dropout=0.1, context_size=None, residual=False):
+    def __init__(
+        self,
+        input_size: int,
+        hidden_size: int,
+        output_size: int,
+        dropout: float = 0.1,
+        context_size: int = None,
+        residual: bool = False,
+    ):
         super().__init__()
         self.input_size = input_size
         self.output_size = output_size
@@ -130,7 +138,7 @@ class GatedResidualNetwork(nn.Module):
     def forward(self, x, context=None, residual=None):
         if self.input_size != self.output_size and residual is None:
             residual = self.skip_layer(x)
-        elif residual is None:
+        if residual is None:
             residual = x
 
         if self.input_size == 1 and self.hidden_size == 1:
@@ -150,9 +158,9 @@ class VariableSelectionNetwork(nn.Module):
     def __init__(
         self,
         input_sizes: Dict[str, int],
-        hidden_size,
-        dropout=0.1,
-        context_size=None,
+        hidden_size: int,
+        dropout: float = 0.1,
+        context_size: int = None,
         single_variable_grns: Dict[str, GatedResidualNetwork] = {},
     ):
         """
@@ -257,7 +265,7 @@ class PositionalEncoder(torch.nn.Module):
 
 
 class ScaledDotProductAttention(nn.Module):
-    def __init__(self, dropout=0.0, scale=True):
+    def __init__(self, dropout: float = 0.0, scale: bool = True):
         super(ScaledDotProductAttention, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
         self.softmax = nn.Softmax(dim=2)
@@ -278,7 +286,7 @@ class ScaledDotProductAttention(nn.Module):
 
 
 class InterpretableMultiHeadAttention(nn.Module):
-    def __init__(self, n_head, d_model, dropout=0.0):
+    def __init__(self, n_head: int, d_model: int, dropout: float = 0.0):
         super(InterpretableMultiHeadAttention, self).__init__()
 
         self.n_head = n_head
@@ -301,7 +309,7 @@ class InterpretableMultiHeadAttention(nn.Module):
             else:
                 torch.nn.init.zeros_(p)
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v, mask=None) -> Tuple[torch.Tensor, torch.Tensor]:
 
         heads = []
         attns = []