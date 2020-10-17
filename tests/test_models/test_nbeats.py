a43d390a0c6c6fb99469b7357575dcb34eab8bee
Jan Beitner <beitner.jan@bcg.com>
2020-10-17 12:06:59 +0100

    fix typo and test pickling of data and models

--- tests/test_models/test_nbeats.py
+++ tests/test_models/test_nbeats.py
@@ -1,5 +1,7 @@
+import pickle
 import shutil
 
+import pytest
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -7,9 +9,9 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_forecasting.models import NBeats
 
 
-def test_integration(dataloaders_fixed_window_without_coveratiates, tmp_path, gpus):
-    train_dataloader = dataloaders_fixed_window_without_coveratiates["train"]
-    val_dataloader = dataloaders_fixed_window_without_coveratiates["val"]
+def test_integration(dataloaders_fixed_window_without_covariates, tmp_path, gpus):
+    train_dataloader = dataloaders_fixed_window_without_covariates["train"]
+    val_dataloader = dataloaders_fixed_window_without_covariates["val"]
     early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=1, verbose=False, mode="min")
 
     logger = TensorBoardLogger(tmp_path)
@@ -50,3 +52,22 @@ def test_integration(dataloaders_fixed_window_without_coveratiates, tmp_path, gp
         shutil.rmtree(tmp_path, ignore_errors=True)
 
     net.predict(val_dataloader, fast_dev_run=True, return_index=True, return_decoder_lengths=True)
+
+
+@pytest.fixture
+def model(dataloaders_fixed_window_without_covariates):
+    dataset = dataloaders_fixed_window_without_covariates["train"].dataset
+    net = NBeats.from_dataset(
+        dataset,
+        learning_rate=0.15,
+        log_gradient_flow=True,
+        widths=[4, 4, 4],
+        log_interval=1000,
+        backcast_loss_ratio=1.0,
+    )
+    return net
+
+
+def test_pickle(model):
+    pkl = pickle.dumps(model)
+    pickle.loads(pkl)
