a362adcea8e3215e252bba91b141e22ab94994b7
Jan Beitner <beitner.jan@bcg.com>
2020-10-25 09:58:19 +0000

    Work toward encoder decoder model for deepar

--- pytorch_forecasting/models/deepar/__init__.py
+++ pytorch_forecasting/models/deepar/__init__.py
@@ -11,6 +11,7 @@ import torch
 import torch.nn as nn
 import torch.distributions as dists
 import numpy as np
+from torch.nn.utils import rnn
 
 from pytorch_forecasting.models.base_model import AutoRegressiveBaseModelWithCovariates
 from pytorch_forecasting.metrics import SMAPE, MAE, RMSE, MAPE, MASE, DistributionLoss, NormalDistributionLoss
@@ -35,6 +36,7 @@ class DeepAR(AutoRegressiveBaseModelWithCovariates):
         embedding_labels: Dict[str, np.ndarray] = {},
         x_reals: List[str] = [],
         x_categoricals: List[str] = [],
+        target: str = None,
         loss: DistributionLoss = None,
         logging_metrics: nn.ModuleList = None,
         **kwargs,
@@ -58,6 +60,7 @@ class DeepAR(AutoRegressiveBaseModelWithCovariates):
         assert set(self.encoder_variables) == set(
             self.decoder_variables
         ), "Encoder and decoder variables have to be the same"
+        assert target in time_varying_reals_decoder, "target has to be real"  # todo: remove this restriction
 
         rnn = getattr(nn, cell_type)
         self.rnn = rnn(
@@ -102,35 +105,91 @@ class DeepAR(AutoRegressiveBaseModelWithCovariates):
             dataset, allowed_encoder_known_variable_names=allowed_encoder_known_variable_names, **new_kwargs
         )
 
+    def construct_input_vector(
+        self, x_cat: torch.Tensor, x_cont: torch.Tensor, one_off_target: torch.Tensor = None
+    ) -> torch.Tensor:
+        """
+        input dimensions: n_samples x time x variables
+        """
+        # create input vector
+        if len(self.categoricals) > 0:
+            embeddings = self.embeddings(x_cat)
+            flat_embeddings = torch.cat([emb for emb in embeddings.values()], dim=-1)
+            input_vector = flat_embeddings
+
+        if len(self.reals) > 0:
+            input_vector = x_cont
+
+        if len(self.reals) > 0 and len(self.categoricals) > 0:
+            input_vector = torch.cat([x_cont, flat_embeddings], dim=-1)
+
+        # shift target by one
+        input_vector[..., self.target_position] = torch.roll(input_vector[..., self.target_position], shifts=1, dim=1)
+
+        if one_off_target is not None:  # set first target input (which is rolled over)
+            input_vector[:, 0, self.target_position] = one_off_target
+        else:
+            input_vector = input_vector[:, 1:]
+
+        # shift target
+        return input_vector
+
+    @property
+    def target_position(self):
+        variables = self.hparams.time_varying_reals_encoder  # todo: support categorical targets
+        pos = variables.index(self.hparams.target)
+        return pos
+
     def forward(self, x: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         """
         input dimensions: n_samples x time x variables
         """
         max_encoder_length = x["encoder_lengths"].max()
-        # create input vector
-        if len(self.categoricals) > 0:
-            x_cat = torch.cat([x["encoder_cat"], x["decoder_cat"]], dim=1)  # concatenate in time dimension
-            embeddings = self.embeddings(x_cat)
-            flat_embeddings = torch.cat([emb for emb in embeddings.values()], dim=-1)
-            input_vector = flat_embeddings
-        if len(self.reals) > 0:
-            x_cont = torch.cat([x["encoder_cont"], x["decoder_cont"]], dim=1)  # concatenate in time dimension
-            input_vector = x_cont
+        assert x["encoder_lengths"].min() > 0
+        # encode using rnn
+        if max_encoder_length > 1:
+            encoder_lengths = x["encoder_lengths"] - 1
+            rnn_encoder_lengths = encoder_lengths.where(encoder_lengths > 0, torch.ones_like(encoder_lengths))
+            input_vector = self.construct_input_vector(x["encoder_cat"], x["encoder_cont"])
+            packed_input = rnn.pack_padded_sequence(
+                input_vector, rnn_encoder_lengths, enforce_sorted=False, batch_first=True
+            )
+            _, (hidden, cell) = self.rnn(packed_input)  # second ouput is not needed (hidden state)
+            # replace hidden cell with initial input if encoder_length is zero to determine correct initial state
+            no_encoding = (encoder_lengths == 0)[None, :, None]  # shape: n_lstm_layers x batch_size x hidden_size
+            hidden = hidden.masked_scatter(no_encoding, 0)
+            cell = cell.masked_scatter(no_encoding, 0)
+        else:
+            hidden = torch.zeros(
+                (x["encoder_cont"].size(0), self.hparams.hidden_size),
+                device=x["decoder_cont"].device,
+                dtype=torch.float,
+            )
+            cell = torch.zeros(
+                (x["encoder_cont"].size(0), self.hparams.hidden_size),
+                device=x["decoder_cont"].device,
+                dtype=torch.float,
+            )
 
-        if len(self.reals) > 0 and len(self.categoricals) > 0:
-            input_vector = torch.cat([flat_embeddings, x_cont], dim=-1)
+        # decode
+        input_vector = self.construct_input_vector(
+            x["decoder_cat"], x["decoder_cont"], one_off_target=x["encoder_cont"][:, -1, self.target_position]
+        )
+        if self.training:
+            decoder_output, _ = self.rnn(
+                rnn.pack_padded_sequence(packed_input, x["decoder_lengths"], enforce_sorted=False, batch_first=True),
+                (hidden, cell),
+            )
+        else:
+            decoder_output = 2
 
-        # run through rnn
-        rnn_output, _ = self.rnn(input_vector)  # second ouput is not needed (hidden state)
-
-        output = self.distribution_projector(rnn_output)
+        output = self.distribution_projector(decoder_output)
 
         # return relevant part
-        returns = dict(
+        return dict(
             encoder_output=output[:, :max_encoder_length],
             prediction=output[:, max_encoder_length:],
             groups=x["groups"],
             decoder_time_idx=x["decoder_time_idx"],
             target_scale=x["target_scale"],
         )
-        return returns
