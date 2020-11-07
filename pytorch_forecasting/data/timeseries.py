bd4e6aca6b68ce7338d9241e7dec6c3366bcfe2f
Jan Beitner <beitner.jan@bcg.com>
2020-11-07 21:40:51 +0000

    x_to_index TimeSeriesDataSet method needs cpu tensors

--- pytorch_forecasting/data/timeseries.py
+++ pytorch_forecasting/data/timeseries.py
@@ -1158,16 +1158,16 @@ class TimeSeriesDataSet(Dataset):
             **kwargs,
         )
 
-    def x_to_index(self, x) -> pd.DataFrame:
+    def x_to_index(self, x: Dict[str, torch.Tensor]) -> pd.DataFrame:
         """
         Decode dataframe index from x.
 
         Returns:
             dataframe with time index column for first prediction and group ids
         """
-        index_data = {self.time_idx: x["decoder_time_idx"][:, 0]}
+        index_data = {self.time_idx: x["decoder_time_idx"][:, 0].cpu()}
         for id in self.group_ids:
-            index_data[id] = x["groups"][:, self.group_ids.index(id)]
+            index_data[id] = x["groups"][:, self.group_ids.index(id)].cpu()
             # decode if possible
             index_data[id] = self.transform_values(id, index_data[id], inverse=True)
         index = pd.DataFrame(index_data)
