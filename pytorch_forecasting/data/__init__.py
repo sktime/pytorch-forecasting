7e040ee1b127670eba2ce774c72ed8c5005c2bbc
Jan Beitner <beitner.jan@bcg.com>
2021-03-04 10:33:35 +0000

    Model comparison and better imports

--- pytorch_forecasting/data/__init__.py
+++ pytorch_forecasting/data/__init__.py
@@ -4,7 +4,13 @@ Datasets, etc. for timeseries data.
 Handling timeseries data is not trivial. It requires special treatment. This sub-package provides the necessary tools
 to abstracts the necessary work.
 """
-from pytorch_forecasting.data.encoders import EncoderNormalizer, GroupNormalizer, NaNLabelEncoder, TorchNormalizer
+from pytorch_forecasting.data.encoders import (
+    EncoderNormalizer,
+    GroupNormalizer,
+    MultiNormalizer,
+    NaNLabelEncoder,
+    TorchNormalizer,
+)
 from pytorch_forecasting.data.timeseries import TimeSeriesDataSet, TimeSynchronizedBatchSampler
 
 __all__ = [
@@ -14,4 +20,5 @@ __all__ = [
     "TorchNormalizer",
     "EncoderNormalizer",
     "TimeSynchronizedBatchSampler",
+    "MultiNormalizer",
 ]
--- pytorch_forecasting/models/__init__.py
+++ pytorch_forecasting/models/__init__.py
@@ -1,10 +1,29 @@
 """
 Models for timeseries forecasting.
 """
-from pytorch_forecasting.models.base_model import BaseModel
+from pytorch_forecasting.models.base_model import (
+    AutoRegressiveBaseModel,
+    AutoRegressiveBaseModelWithCovariates,
+    BaseModel,
+    BaseModelWithCovariates,
+)
 from pytorch_forecasting.models.baseline import Baseline
 from pytorch_forecasting.models.deepar import DeepAR
 from pytorch_forecasting.models.nbeats import NBeats
+from pytorch_forecasting.models.nn import GRU, LSTM, MultiEmbedding, get_rnn
 from pytorch_forecasting.models.temporal_fusion_transformer import TemporalFusionTransformer
 
-__all__ = ["NBeats", "TemporalFusionTransformer", "DeepAR", "BaseModel", "Baseline"]
+__all__ = [
+    "NBeats",
+    "TemporalFusionTransformer",
+    "DeepAR",
+    "BaseModel",
+    "Baseline",
+    "BaseModelWithCovariates",
+    "AutoRegressiveBaseModel",
+    "AutoRegressiveBaseModelWithCovariates",
+    "get_rnn",
+    "LSTM",
+    "GRU",
+    "MultiEmbedding",
+]
