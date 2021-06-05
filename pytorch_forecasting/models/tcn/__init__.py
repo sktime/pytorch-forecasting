0efea90bf409a67ec80547a4cf2fa996aeac186e
wangbingnan136 <42114835+wangbingnan136@users.noreply.github.com>
2021-06-05 08:19:34 +0800

    TCN of pytorch-forecasting

---
+++ pytorch_forecasting/models/tcn/__init__.py
@@ -0,0 +1,176 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+from typing import Dict, List, Tuple
+
+from pytorch_forecasting.models.nn import MultiEmbedding
+from pytorch_forecasting.models import BaseModelWithCovariates
+
+
+class GAP1d(nn.Module):
+    "Global Adaptive Pooling + Flatten"
+    def __init__(self, output_size=1):
+        super(GAP1d,self).__init__()
+        self.gap = nn.AdaptiveAvgPool1d(output_size)
+    def forward(self, x):
+        res=self.gap(x)
+        return res.view(res.size(0), -1) ##fastai's layer has problems with latest torch
+
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+class Flatten(nn.Module):
+  def __init__(self,):
+    super(Flatten,self).__init__()
+  def forward(self,x):
+    return x.view(x.size(0),-1)
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x) ##保持时序的长度不变，但是内部的特征会发生改变
+        return self.relu(out + res)
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+
+
+class TCN(BaseModelWithCovariates):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        n_hidden_layers: List[int],
+        conv_dropout:float,
+        fc_dropout:float,
+        kernel_size:int,
+        x_reals: List[str],
+        x_categoricals: List[str],
+        embedding_sizes: Dict[str, Tuple[int, int]],
+        embedding_labels: Dict[str, List[str]],
+        static_categoricals: List[str],
+        static_reals: List[str],
+        time_varying_categoricals_encoder: List[str],
+        time_varying_categoricals_decoder: List[str],
+        time_varying_reals_encoder: List[str],
+        time_varying_reals_decoder: List[str],
+        embedding_paddings: List[str],
+        categorical_groups: Dict[str, List[str]],
+        training_windows=24,
+        **kwargs,
+    ):
+        # saves arguments in signature to `.hparams` attribute, mandatory call - do not skip this
+        self.save_hyperparameters()
+        
+        # pass additional arguments to BaseModel.__init__, mandatory call - do not skip this
+        super().__init__(**kwargs)
+
+        # create embedder - can be fed with x["encoder_cat"] or x["decoder_cat"] and will return
+        # dictionary of category names mapped to embeddings
+        self.input_embeddings = MultiEmbedding(
+            embedding_sizes=self.hparams.embedding_sizes,
+            categorical_groups=self.hparams.categorical_groups,
+            embedding_paddings=self.hparams.embedding_paddings,
+            x_categoricals=self.hparams.x_categoricals,
+        )
+
+        # calculate the size of all concatenated embeddings + continous variables
+        n_features = sum(
+            embedding_size for classes_size, embedding_size in self.hparams.embedding_sizes.values()
+        ) + len(self.reals)
+
+        # create network that will be fed with continious variables and embeddings
+        self.network = nn.Sequential(TemporalConvNet(
+            num_inputs=n_features,
+            num_channels=self.hparams.n_hidden_layers,
+            kernel_size=self.hparams.kernel_size,
+            dropout=self.hparams.conv_dropout),
+
+            Flatten(),
+            nn.Dropout(self.hparams.fc_dropout),
+            nn.Linear(25*24,self.hparams.output_size)
+            
+        )
+
+
+    def forward(self, x: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # x is a batch generated based on the TimeSeriesDataset
+        batch_size = x["encoder_lengths"].size(0)
+        embeddings = self.input_embeddings(x["encoder_cat"])  # returns dictionary with embedding tensors
+        network_input = torch.cat(
+            [x["encoder_cont"]]
+            + [
+                emb
+                for name, emb in embeddings.items()
+                if name in self.encoder_variables or name in self.static_variables
+            ],
+            dim=-1,
+        )
+        prediction = self.network(network_input.permute(0,2,1))
+
+        # We need to return a dictionary that at least contains the prediction and the target_scale.
+        # The parameter can be directly forwarded from the input.
+        return dict(prediction=prediction, target_scale=x["target_scale"])
+
+    @classmethod
+    def from_dataset(cls, dataset: TimeSeriesDataSet, **kwargs):
+        new_kwargs = {
+            "output_size": dataset.max_prediction_length,
+            "input_size": dataset.max_encoder_length,
+        }
+        new_kwargs.update(kwargs)  # use to pass real hyperparameters and override defaults set by dataset
+        # example for dataset validation
+        assert dataset.max_prediction_length == dataset.min_prediction_length, "Decoder only supports a fixed length"
+        assert dataset.min_encoder_length == dataset.max_encoder_length, "Encoder only supports a fixed length"
+
+        return super().from_dataset(dataset, **new_kwargs)
+
