30701ff1c64b323a8085ff8461a06b842f83ac36
mikcnt <11929535+mikcnt@users.noreply.github.com>
2022-01-02 15:43:00 +0100

    Add `ForecastingLoggerBase` to init

--- pytorch_forecasting/loggers/__init__.py
+++ pytorch_forecasting/loggers/__init__.py
@@ -1,8 +1,8 @@
 """
 Forecasting loggers for timeseries forecasting.
 """
-
+from pytorch_forecasting.loggers.base_logger import ForecastingLoggerBase
 from pytorch_forecasting.loggers.tensorboard_logger import ForecastingTensorBoardLogger
 from pytorch_forecasting.loggers.wandb_logger import ForecastingWandbLogger
 
-__all__ = ["ForecastingTensorBoardLogger", "ForecastingWandbLogger"]
+__all__ = ["ForecastingLoggerBase", "ForecastingTensorBoardLogger", "ForecastingWandbLogger"]
