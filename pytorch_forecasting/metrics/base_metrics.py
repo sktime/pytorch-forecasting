16f0d3a6511160b5c166346b18cde248882e88ee
German <germikgerman@gmail.com>
2024-01-21 19:58:35 +0300

    Update base_metrics.py
    infinite loss bug fixed
    
    infinite loss bug fixed

--- pytorch_forecasting/metrics/base_metrics.py
+++ pytorch_forecasting/metrics/base_metrics.py
@@ -799,7 +799,7 @@ class MultiHorizonMetric(Metric):
         else:
             losses = losses.sum()
             if not torch.isfinite(losses):
-                losses = torch.tensor(1e9, device=losses.device)
+                losses = losses.fill_(1e9)
                 warnings.warn("Loss is not finite. Resetting it to 1e9")
             self.losses = self.losses + losses
             self.lengths = self.lengths + lengths.sum()
