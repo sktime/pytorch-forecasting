940ce81c4361e13d411cdbff08220d3ef6dd0887
Luke-Chesley <lukechesleyfive@gmail.com>
2024-02-17 09:07:57 -0500

    modified:   pytorch_forecasting/utils.py

--- pytorch_forecasting/utils.py
+++ pytorch_forecasting/utils.py
@@ -246,7 +246,7 @@ def concat_sequences(
     if isinstance(sequences[0], rnn.PackedSequence):
         return rnn.pack_sequence(sequences, enforce_sorted=False)
     elif isinstance(sequences[0], torch.Tensor):
-        return torch.cat(sequences, dim=0)
+        return torch.cat(sequences, dim=1)
     elif isinstance(sequences[0], (tuple, list)):
         return tuple(
             concat_sequences([sequences[ii][i] for ii in range(len(sequences))]) for i in range(len(sequences[0]))
