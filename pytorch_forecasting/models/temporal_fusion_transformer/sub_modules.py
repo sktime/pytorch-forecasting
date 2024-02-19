d93c1e0f8369237e0c502ce67980adedf18359ad
Marcrb2 <44782012+Marcrb2@users.noreply.github.com>
2024-02-19 10:09:47 +0100

    Allow mixed precision TFT training

--- pytorch_forecasting/models/temporal_fusion_transformer/sub_modules.py
+++ pytorch_forecasting/models/temporal_fusion_transformer/sub_modules.py
@@ -388,7 +388,7 @@ class ScaledDotProductAttention(nn.Module):
             attn = attn / dimension
 
         if mask is not None:
-            attn = attn.masked_fill(mask, -1e9)
+            attn = attn.masked_fill(mask, -float("inf"))
         attn = self.softmax(attn)
 
         if self.dropout is not None:
