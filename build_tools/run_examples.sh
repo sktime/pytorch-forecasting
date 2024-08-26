c73ed8a7343cc556f5c7687b1a7e5ab9cb60ea16
Xinyu-Wu-0000 <xinyu.wuxinyu@outlook.com>
2024-08-26 10:45:46 +0800

    test which python in sh file

--- build_tools/run_examples.sh
+++ build_tools/run_examples.sh
@@ -3,8 +3,8 @@
 # Script to run all example notebooks.
 # copy-paste from sktime's run_examples.sh
 set -euxo pipefail
-
-CMD="jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=600"
+echo $(which python)
+CMD="python -m jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=600"
 
 for notebook in docs/source/tutorials/*.ipynb; do
   echo "Running: $notebook"
