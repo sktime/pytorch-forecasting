63d20d3f9b0565bbb05ff796a9959540db6a432f
Franz Király <f.kiraly@ucl.ac.uk>
2024-08-30 19:53:49 +0100

    Update test_temporal_fusion_transformer.py

--- tests/test_models/test_temporal_fusion_transformer.py
+++ tests/test_models/test_temporal_fusion_transformer.py
@@ -25,7 +25,6 @@ from pytorch_forecasting.models import TemporalFusionTransformer
 from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimize_hyperparameters
 from pytorch_forecasting.utils._dependencies import _get_installed_packages
 
-
 if sys.version.startswith("3.6"):  # python 3.6 does not have nullcontext
     from contextlib import contextmanager
 
