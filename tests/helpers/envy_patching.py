b58b7014522301aa24b321c8f34b381a398986a9
hirwa <hirwanshutiflx@gmail.com>
2024-09-04 16:57:22 +0530

    fix the decorator

--- tests/helpers/envy_patching.py
+++ tests/helpers/envy_patching.py
@@ -31,16 +31,11 @@ def monkeypatch_env(key, value):
     def decorator(test_func):
         @wraps(test_func)
         def wrapper(*args, **kwargs):
-            @pytest.fixture
-            def _monkeypatch_env(monkeypatch):
-                monkeypatch.setenv(key, value)
+            monkeypatch = kwargs.get("monkeypatch", pytest.MonkeyPatch())
+            monkeypatch.setenv(key, value)
 
-            @pytest.mark.usefixtures("_monkeypatch_env")
-            def run_test():
-                return test_func(*args, **kwargs)
+            return test_func(*args, **kwargs)
 
-            return run_test()
-
-        return wrapper
+        return pytest.mark.usefixtures("monkeypatch")(wrapper)
 
     return decorator
