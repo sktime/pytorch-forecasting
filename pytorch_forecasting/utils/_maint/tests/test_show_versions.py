0db6f4770a750d57870a05be951ef523c9538ce1
Franz Király <f.kiraly@ucl.ac.uk>
2024-09-25 09:32:19 +0100

    linting

--- pytorch_forecasting/utils/_maint/tests/test_show_versions.py
+++ pytorch_forecasting/utils/_maint/tests/test_show_versions.py
@@ -4,11 +4,7 @@ import pathlib
 import uuid
 
 from pytorch_forecasting.utils._dependencies import _check_soft_dependencies
-from pytorch_forecasting.utils._maint._show_versions import (
-    DEFAULT_DEPS_TO_SHOW,
-    _get_deps_info,
-    show_versions,
-)
+from pytorch_forecasting.utils._maint._show_versions import DEFAULT_DEPS_TO_SHOW, _get_deps_info, show_versions
 
 
 def test_show_versions_runs():
