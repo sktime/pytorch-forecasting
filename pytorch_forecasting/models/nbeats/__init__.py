0aac3e27852e8aefc7fe2a30173d7da2eca70935
Franz Király <fkiraly@gcos.ai>
2024-12-26 01:16:42 +0100

    [ENH] refactor `__init__` modules to no longer contain classes (#1738)
    This PR refactors the `__init__` modules in `models`, moving any content
    such as neural network classes to submodules.
    
    Imports will continue working unchanged, as the native contents of the
    module are still exported.
    
    This is preparing a further structural refactors.
    
    The moved files are also not changed, in order to retain the commit as
    one that moves files, hence retaining edit history. In order for this to
    take effect on `main`, PR
    https://github.com/sktime/pytorch-forecasting/pull/1739 must be merged
    first (even though this will result in an intermediate, non-passing
    state).

---
+++ pytorch_forecasting/models/nbeats/__init__.py
@@ -0,0 +1,6 @@
+"""N-Beats model for timeseries forecasting without covariates."""
+
+from pytorch_forecasting.models.nbeats._nbeats import NBeats
+from pytorch_forecasting.models.nbeats.sub_modules import NBEATSGenericBlock, NBEATSSeasonalBlock, NBEATSTrendBlock
+
+__all__ = ["NBeats", "NBEATSGenericBlock", "NBEATSSeasonalBlock", "NBEATSTrendBlock"]
---
+++ pytorch_forecasting/models/rnn/__init__.py
@@ -0,0 +1,5 @@
+"""Simple recurrent model - either with LSTM or GRU cells."""
+
+from pytorch_forecasting.models.rnn._rnn import RecurrentNetwork
+
+__all__ = ["RecurrentNetwork"]
