5d3dbde9e4fc9d6d3bf8ebbeac8be975f560cef5
Franz Király <f.kiraly@ucl.ac.uk>
2025-01-05 00:28:27 +0100

    Revert "Update timeseries.py"
    This reverts commit b22e4d9925ffd618080a335c15fbddaf6cfa9574.

--- pytorch_forecasting/data/timeseries.py
+++ pytorch_forecasting/data/timeseries.py
@@ -1402,8 +1402,6 @@ class TimeSeriesDataSet(Dataset):
             if not isinstance(cols, list) and cols not in data.columns:
                 return None
             if isinstance(cols, list):
-                if len(cols) == 0:
-                    return []
                 dtypekind = data.dtypes[cols[0]].kind
             else:
                 dtypekind = data.dtypes[cols].kind
