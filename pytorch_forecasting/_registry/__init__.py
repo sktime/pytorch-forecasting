02b0ce6fa53443044fffce8cbbce54a0c6d6b947
Franz Király <f.kiraly@ucl.ac.uk>
2025-02-22 23:33:58 +0100

    skeleton

--- pytorch_forecasting/_registry/__init__.py
+++ pytorch_forecasting/_registry/__init__.py
@@ -1,16 +1,5 @@
 """PyTorch Forecasting registry."""
 
-from pytorch_forecasting._registry._lookup import all_objects, all_tags
-from pytorch_forecasting._registry._tags import (
-    OBJECT_TAG_LIST,
-    OBJECT_TAG_REGISTER,
-    check_tag_is_valid,
-)
+from pytorch_forecasting._registry._lookup import all_objects
 
-__all__ = [
-    "OBJECT_TAG_LIST",
-    "OBJECT_TAG_REGISTER",
-    "all_objects",
-    "all_tags",
-    "check_tag_is_valid",
-]
+__all__ = ["all_objects"]
