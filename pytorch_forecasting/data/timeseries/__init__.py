3edb08b7ea1b97d06b47b0ebcc83aaef9bec8083
Franz Király <fkiraly@gcos.ai>
2025-05-01 14:33:17 +0200

    Update __init__.py

--- pytorch_forecasting/data/timeseries/__init__.py
+++ pytorch_forecasting/data/timeseries/__init__.py
@@ -1,9 +1,9 @@
 """Data loaders for time series data."""
 
 from pytorch_forecasting.data.timeseries._timeseries import (
+    TimeSeriesDataSet,
     _find_end_indices,
     check_for_nonfinite,
-    TimeSeriesDataSet,
 )
 from pytorch_forecasting.data.timeseries._timeseries_v2 import TimeSeries
 
