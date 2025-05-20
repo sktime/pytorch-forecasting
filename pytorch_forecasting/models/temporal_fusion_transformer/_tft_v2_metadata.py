f18e09d183f40608d9d92e5cb7ef50d02cbf4644
Aryan Saini <starktony.2032@gmail.com>
2025-05-21 01:16:44 +0530

    add TFTMetadata class

---
+++ pytorch_forecasting/models/temporal_fusion_transformer/_tft_v2_metadata.py
@@ -0,0 +1,24 @@
+"""TFT metadata container."""
+
+from pytorch_forecasting.models.base._base_object import _BasePtForecaster
+
+
+class TFTMetadata(_BasePtForecaster):
+    """TFT metadata container."""
+
+    _tags = {
+        "info:name": "TFT",
+        "info:compute": 3,
+        "authors": ["jdb78"],
+        "capability:exogenous": True,
+        "capability:multivariate": True,
+        "capability:pred_int": True,
+        "capability:flexible_history_length": False,
+    }
+
+    @classmethod
+    def get_model_cls(cls):
+        """Get model class."""
+        from pytorch_forecasting.models.temporal_fusion_transformer._tft_v2 import TFT
+
+        return TFT
