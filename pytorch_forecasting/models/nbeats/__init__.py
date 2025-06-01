c25d03fd24c96c61e818bd60171148d9dc8e7e9c
Franz Király <fkiraly@gcos.ai>
2025-06-01 08:10:14 +0200

    [ENH] refactor test metadata container to include data loader configs (#1861)
    Refactors the test metadata container to include data loader configs
    which may vary, adds this in a private class method
    `_get_test_dataloaders_from`. Also refactors the `_integration` method
    in `TestAllEstimators` accordingly.
    
    Also:
    
    * Adds `NBeats` metadata container for v1 tests.
    * Removes the private import from `test_tide`, of `_integration` from
    `test_all_estimators`

--- pytorch_forecasting/models/nbeats/__init__.py
+++ pytorch_forecasting/models/nbeats/__init__.py
@@ -1,10 +1,17 @@
 """N-Beats model for timeseries forecasting without covariates."""
 
 from pytorch_forecasting.models.nbeats._nbeats import NBeats
+from pytorch_forecasting.models.nbeats._nbeats_metadata import NBeatsMetadata
 from pytorch_forecasting.models.nbeats.sub_modules import (
     NBEATSGenericBlock,
     NBEATSSeasonalBlock,
     NBEATSTrendBlock,
 )
 
-__all__ = ["NBeats", "NBEATSGenericBlock", "NBEATSSeasonalBlock", "NBEATSTrendBlock"]
+__all__ = [
+    "NBeats",
+    "NBEATSGenericBlock",
+    "NBeatsMetadata",
+    "NBEATSSeasonalBlock",
+    "NBEATSTrendBlock",
+]
