1829de58a9d8c743e7830da251f1f117f0556082
Pranav Bhat <pranavbhat2004@gmail.com>
2025-06-09 15:53:15 +0530

    restore v1 version of _timexer

--- pytorch_forecasting/models/timexer/__init__.py
+++ pytorch_forecasting/models/timexer/__init__.py
@@ -1,5 +1,31 @@
-"""TimeXer model."""
+"""
+TimeXer model for forecasting time series.
+"""
 
 from pytorch_forecasting.models.timexer._timexer import TimeXer
+from pytorch_forecasting.models.timexer._timexer_metadata import TimeXerMetadata
+from pytorch_forecasting.models.timexer.sub_modules import (
+    AttentionLayer,
+    DataEmbedding_inverted,
+    Encoder,
+    EncoderLayer,
+    EnEmbedding,
+    FlattenHead,
+    FullAttention,
+    PositionalEmbedding,
+    TriangularCausalMask,
+)
 
-__all__ = ["TimeXer"]
+__all__ = [
+    "TimeXer",
+    "TriangularCausalMask",
+    "FullAttention",
+    "AttentionLayer",
+    "DataEmbedding_inverted",
+    "PositionalEmbedding",
+    "FlattenHead",
+    "EnEmbedding",
+    "Encoder",
+    "EncoderLayer",
+    "TimeXerMetadata",
+]
