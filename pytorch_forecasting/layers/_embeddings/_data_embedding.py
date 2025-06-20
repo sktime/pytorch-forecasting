33041c74f5598be629cf02dbca87da2195fd622e
Pranav Bhat <63770398+PranavBhatP@users.noreply.github.com>
2025-06-20 17:47:17 +0530

    [ENH] Implementing D2 data module, tests and `TimeXer` model from `tslib`  for v2 (#1836)
    ### Description
    
    This PR fixes issue #1833.
    
    This PR involves the following changes:
    
    - New dataset and datamodule for `tslib` -
    [link](https://github.com/sktime/pytorch-forecasting/pull/1836/files#diff-bcb65c03ac714a1561901abf528b65d4754b327905fa590f31a15467629ef680)
    - Implementation of `TimeXer` with new data module. -
    [link](https://github.com/sktime/pytorch-forecasting/pull/1836/files#diff-81d7988c60658a6206368bbfd48f92ce2efb53883f0d6d52ecf4c92c68546d8b)
    - Base class implementation of a `TslibBaseModel` -
    [link](https://github.com/sktime/pytorch-forecasting/pull/1836/files#diff-05f662f6e4229d72f94c90fbf70cc3fa007833c69681fb18e47220c5b7df5919).
    - Tests for the `tslib` data module. -
    [link](https://github.com/sktime/pytorch-forecasting/pull/1836/files#diff-8a5a7aaf7fcdb0efa90272d5bdba449dabea7d25ec15321c3228dc983749e5dc)
    - Example notebook -
    [link](https://github.com/sktime/pytorch-forecasting/pull/1836/files#diff-1c7358b4322a7125a2145077588b15232fe2bfb7b23063267ea2638578ad8658)
    - Restructure codebase to include a layers directory for module
    containing architectural deep learning layer classes.
    - Metadata container for _timexer.py using the v2 framework -
    [link](https://github.com/sktime/pytorch-forecasting/pull/1836/files#diff-f2ce653a26538074931d0d28310c3f42ba52beb95ebd854739d678abb1203741)

---
+++ pytorch_forecasting/layers/_embeddings/_data_embedding.py
@@ -0,0 +1,38 @@
+"""
+Data embedding layer for exogenous variables.
+"""
+
+import math
+from math import sqrt
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DataEmbedding_inverted(nn.Module):
+    """
+    Data embedding module for time series data.
+    Args:
+        c_in (int): Number of input features.
+        d_model (int): Dimension of the model.
+        embed_type (str): Type of embedding to use. Defaults to "fixed".
+        freq (str): Frequency of the time series data. Defaults to "h".
+        dropout (float): Dropout rate. Defaults to 0.1.
+    """
+
+    def __init__(self, c_in, d_model, dropout=0.1):
+        super().__init__()
+        self.value_embedding = nn.Linear(c_in, d_model)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x, x_mark):
+        x = x.permute(0, 2, 1)
+        # x: [Batch Variate Time]
+        if x_mark is None:
+            x = self.value_embedding(x)
+        else:
+            x = self.value_embedding(torch.cat([x, x_mark.permute(0, 2, 1)], 1))
+        # x: [Batch Variate d_model]
+        return self.dropout(x)
