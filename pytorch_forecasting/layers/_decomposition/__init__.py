b2cfc147d515f4bd10ac87ff524de5a10e3e7b8f
Pranav Bhat <63770398+PranavBhatP@users.noreply.github.com>
2025-06-24 03:44:58 +0530

    [ENH] `DLinear` model from `tslib` (#1874)
    ### Description
    
    This PR works on issue #1869 to implement `DLinear` from `tslib`. Stacks
    on #1836 and #1841.
    
    Files to review (just pointing this out since there's a lot of files in
    the stack, not relevant to this PR).
    - dlinear model -
    [link](https://github.com/sktime/pytorch-forecasting/blob/7f0495d6875ce918a9ffca1bb1ea895313ec81bb/pytorch_forecasting/models/dlinear/_dlinear.py)
    - layers -
    [link](https://github.com/sktime/pytorch-forecasting/blob/7f0495d6875ce918a9ffca1bb1ea895313ec81bb/pytorch_forecasting/layers/decomposition/_series_decomp.py)

---
+++ pytorch_forecasting/layers/_decomposition/__init__.py
@@ -0,0 +1,9 @@
+"""
+Decomposition layers for PyTorch Forecasting.
+"""
+
+from pytorch_forecasting.layers._decomposition._series_decomp import SeriesDecomposition
+
+__all__ = [
+    "SeriesDecomposition",
+]
---
+++ pytorch_forecasting/layers/_filter/__init__.py
@@ -0,0 +1,9 @@
+"""
+Filtering layers for time series forecasting models.
+"""
+
+from pytorch_forecasting.layers._filter._moving_avg_filter import MovingAvg
+
+__all__ = [
+    "MovingAvg",
+]
