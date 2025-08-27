c3283ce08b83f9faaabf1f1f46ae55e1045ed2b0
PranavBhatP <pranavbhat2004@gmail.com>
2025-08-27 18:23:36 +0530

    add raw implementation of scaled_dot_product_attention into model class to fix test failures

--- pytorch_forecasting/models/samformer/_samformer_v2.py
+++ pytorch_forecasting/models/samformer/_samformer_v2.py
@@ -3,6 +3,7 @@ Samformer Model from DSIPTS for PyTorch Forecasting
 ---------------------------------------------------
 """
 
+import math
 from typing import Optional, Union
 
 import numpy as np
@@ -100,6 +101,42 @@ class Samformer(BaseModel):
             self.max_encoder_length, self.max_prediction_length
         )  # noqa: E501
 
+    def _scaled_dot_product_attention(
+        self,
+        query,
+        key,
+        value,
+        attn_mask=None,
+        dropout_p=0.0,
+        is_causal=False,
+        scale=None,
+        enable_gqa=False,
+    ) -> torch.Tensor:
+        L, S = query.size(-2), key.size(-2)
+        scale_factor = 1 / math.sqrt(query.size(-1)) if scale is None else scale
+        attn_bias = torch.zeros(L, S, dtype=query.dtype, device=query.device)
+        if is_causal:
+            assert attn_mask is None
+            temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
+            attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
+            attn_bias.to(query.dtype)
+
+        if attn_mask is not None:
+            if attn_mask.dtype == torch.bool:
+                attn_bias.masked_fill_(attn_mask.logical_not(), float("-inf"))
+            else:
+                attn_bias = attn_mask + attn_bias
+
+        if enable_gqa:
+            key = key.repeat_interleave(query.size(-3) // key.size(-3), -3)
+            value = value.repeat_interleave(query.size(-3) // value.size(-3), -3)
+
+        attn_weight = query @ key.transpose(-2, -1) * scale_factor
+        attn_weight += attn_bias
+        attn_weight = torch.softmax(attn_weight, dim=-1)
+        attn_weight = torch.dropout(attn_weight, dropout_p, train=True)
+        return attn_weight @ value
+
     def forward(self, x: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         """
         Forward pass of the model.
@@ -128,7 +165,7 @@ class Samformer(BaseModel):
         keys = self.compute_keys(x_norm)
         values = self.compute_values(x_norm)
 
-        att_score = nn.functional.scaled_dot_product_attention(queries, keys, values)
+        att_score = self._scaled_dot_product_attention(queries, keys, values)
 
         out = x_norm + att_score
         out = self.linear_forecaster(out)
