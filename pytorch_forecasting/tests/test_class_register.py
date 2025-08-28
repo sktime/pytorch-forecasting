b9f91fe3a276adda44cfc5f46179c45c17b919b7
Franz Király <fkiraly@gcos.ai>
2025-08-28 09:09:07 +0200

    Update test_class_register.py

--- pytorch_forecasting/tests/test_class_register.py
+++ pytorch_forecasting/tests/test_class_register.py
@@ -49,11 +49,11 @@ def get_test_classes_for_obj(obj):
 
     testclass_dict = get_test_class_registry()
 
-    try:
+    if hasattr(obj, "get_class_tag"):
         obj_scitypes = obj.get_class_tag("object_type")
         if not isinstance(obj_scitypes, (list, tuple, set)):
             obj_scitypes = [obj_scitypes]
-    except Exception:
+    else:
         obj_scitypes = []
 
     test_clss = []
