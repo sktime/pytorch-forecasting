086132289535219e456a0448616202a8ac0ee11b
Sohaib-Ahmed21 <sohaibahmed1919@gmail.com>
2025-09-01 12:17:50 -0700

    Add compatibility imports for NBEATS' blocks

--- pytorch_forecasting/models/nbeats/__init__.py
+++ pytorch_forecasting/models/nbeats/__init__.py
@@ -1,5 +1,15 @@
-"""N-Beats model for timeseries forecasting without covariates."""
+"""
+N-Beats model for timeseries forecasting without covariates.
 
+# TODO v2: remove compatibility imports, kept to avoid breaking existing code.
+"""
+
+# Import blocks from new location for backward compatibility
+from pytorch_forecasting.layers._nbeats._blocks import (
+    NBEATSGenericBlock,
+    NBEATSSeasonalBlock,
+    NBEATSTrendBlock,
+)
 from pytorch_forecasting.models.nbeats._grid_callback import GridUpdateCallback
 from pytorch_forecasting.models.nbeats._nbeats import NBeats
 from pytorch_forecasting.models.nbeats._nbeats_adapter import NBeatsAdapter
@@ -12,6 +22,9 @@ __all__ = [
     "NBeatsKAN",
     "NBeats_pkg",
     "NBeatsKAN_pkg",
+    "NBEATSGenericBlock",
+    "NBEATSSeasonalBlock",
+    "NBEATSTrendBlock",
     "NBeatsAdapter",
     "GridUpdateCallback",
 ]
