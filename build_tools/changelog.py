631c5e915705b32a858d97da7fa34f6fa0705152
Aryan Saini <starktony.2032@gmail.com>
2026-01-11 19:23:52 +0530

    Update changelog.py

--- build_tools/changelog.py
+++ build_tools/changelog.py
@@ -118,7 +118,7 @@ def render_contributors(prs: list, fmt: str = "rst"):
     """Find unique authors and print a list in  given format."""
     authors = sorted({pr["user"]["login"] for pr in prs}, key=lambda x: x.lower())
 
-    header = "Contributors"
+    header = "All Contributors"
     if fmt == "github":
         print(f"### {header}")
         print(", ".join(f"@{user}" for user in authors))
@@ -166,7 +166,7 @@ def render_changelog(prs, assigned):
     for title, _ in assigned.items():
         pr_group = [prs[i] for i in assigned[title]]
         if pr_group:
-            print(f"\n## {title}\n")
+            print(f"\n### {title}\n")
 
             for pr in sorted(pr_group, key=lambda x: parser.parse(x["merged_at"])):
                 render_row(pr)
