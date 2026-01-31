d5d598782e0aa1b304d73501939e3b62b50ef48f
Palak Bakshi <palak.bakshi09@gmail.com>
2026-01-31 18:40:37 +0530

    Add N-BEATS v2 model and tests

---
+++ pytorch_forecasting/models/nbeats/_nbeats_pkg_v2.py
@@ -0,0 +1,27 @@
+from pytorch_forecasting.models.base._base_object import _BasePtForecaster
+
+class NBEATS_pkg_v2(_BasePtForecaster):
+    """N-BEATS v2 package container."""
+
+    _tags = {
+        "info:name": "NBeats",
+        "info:compute": 1,
+        "info:pred_type": ["point"],
+        "info:y_type": ["numeric"],
+        "authors": ["jdb78"],
+        "capability:exogenous": False,
+        "capability:multivariate": False,
+        "capability:pred_int": False,
+        "capability:flexible_history_length": False,
+        "capability:cold_start": False,
+    }
+
+    @classmethod
+    def get_cls(cls):
+        """Get model class."""
+        from pytorch_forecasting.models.nbeats._nbeats_v2 import NBEATS_v2
+        return NBEATS_v2
+
+    @classmethod
+    def get_base_test_params(cls):
+        return [{}, {"backcast_loss_ratio": 1.0}]
