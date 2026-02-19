164de4d31b7f008642f5c1b63562d3d3183acda5
Nidhicodes <nidhisingh200418@gmail.com>
2026-02-19 23:45:35 +0530

    code review acknowledged

--- pytorch_forecasting/tests/test_data/test_timeseries_zero_decoder.py
+++ pytorch_forecasting/tests/test_data/test_timeseries_zero_decoder.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import pytest
 import torch
 
 from pytorch_forecasting.data.timeseries import TimeSeriesDataSet
+
 
 def test_timeseries_dataset_zero_prediction_length():
     """Test TimeSeriesDataSet with zero prediction length."""
@@ -28,10 +28,10 @@ def test_timeseries_dataset_zero_prediction_length():
         time_varying_known_reals=["time_idx"],
         time_varying_unknown_reals=["value"],
     )
-    
+
     assert dataset.min_prediction_length == 0
     assert dataset.max_prediction_length == 0
-    
+
     # Test __getitem__
     x, y = dataset[0]
     assert x["encoder_length"] == 5
@@ -40,7 +40,7 @@ def test_timeseries_dataset_zero_prediction_length():
     assert x["x_cont"].shape == (5, 2)
     assert x["encoder_target"].shape == (5,)
     assert y[0].shape == (0,)
-    
+
     # Test dataloader
     dataloader = dataset.to_dataloader(batch_size=4)
     batch = next(iter(dataloader))
@@ -49,6 +49,7 @@ def test_timeseries_dataset_zero_prediction_length():
     assert batch[0]["decoder_lengths"].shape == (4,)
     assert (batch[0]["decoder_lengths"] == 0).all()
     assert batch[0]["decoder_target"].shape == (4, 0)
+
 
 def test_timeseries_dataset_predict_mode_zero_prediction_length():
     """Test TimeSeriesDataSet in predict mode with zero prediction length."""
@@ -71,9 +72,9 @@ def test_timeseries_dataset_predict_mode_zero_prediction_length():
         max_prediction_length=0,
         time_varying_known_reals=["time_idx"],
         time_varying_unknown_reals=["value"],
-        predict_mode=True
+        predict_mode=True,
     )
-    
+
     assert len(dataset) == 2
     for i in range(len(dataset)):
         x, y = dataset[i]
@@ -81,6 +82,7 @@ def test_timeseries_dataset_predict_mode_zero_prediction_length():
         assert x["encoder_length"] == 5
         # Should be at the end of the series
         assert x["encoder_time_idx_start"] + x["encoder_length"] - 1 == 14
+
 
 def test_timeseries_dataset_mixed_prediction_length_including_zero():
     """Test TimeSeriesDataSet with min_prediction_length=0 and max > 0."""
@@ -103,9 +105,9 @@ def test_timeseries_dataset_mixed_prediction_length_including_zero():
         max_prediction_length=2,
         time_varying_known_reals=["time_idx"],
         time_varying_unknown_reals=["value"],
-        predict_mode=False
+        predict_mode=False,
     )
-    
+
     # Verify that some samples have 0 decoder length
     decoder_lengths = [dataset[i][0]["decoder_length"] for i in range(len(dataset))]
     assert 0 in decoder_lengths
