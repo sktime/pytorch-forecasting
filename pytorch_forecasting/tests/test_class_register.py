c6448b62b1c11492c8fee0904f92bc111bb3e8b7
Faakhir30 <zahidfaakhir@gmail.com>
2026-06-17 03:59:45 +0500

    register test_all_data_module
    Signed-off-by: Faakhir30 <zahidfaakhir@gmail.com>

--- pytorch_forecasting/tests/test_class_register.py
+++ pytorch_forecasting/tests/test_class_register.py
@@ -23,10 +23,12 @@ def get_test_class_registry():
     from pytorch_forecasting.tests.test_all_v2.test_all_estimators_v2 import (
         TestAllPtForecastersV2,
     )
+    from pytorch_forecasting.tests.test_all_data_modules import TestAllDataModules
 
     testclass_dict = dict()
     testclass_dict["forecaster_pytorch_v1"] = TestAllPtForecasters
     testclass_dict["forecaster_pytorch_v2"] = TestAllPtForecastersV2
+    testclass_dict["datamodule_v2"] = TestAllDataModules
 
     return testclass_dict
 
