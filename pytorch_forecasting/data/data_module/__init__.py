63ef6f683d9fdef50e4044353c0d34347a63c903
Faakhir30 <zahidfaakhir@gmail.com>
2026-06-17 03:54:35 +0500

    update dm exports
    Signed-off-by: Faakhir30 <zahidfaakhir@gmail.com>

--- pytorch_forecasting/data/data_module/__init__.py
+++ pytorch_forecasting/data/data_module/__init__.py
@@ -1,8 +1,25 @@
 """Data Modules (D2 Layer) of pytorch-forecasting v2"""
 
+from pytorch_forecasting.data.data_module._base_data_module import (
+    BaseTimeSeriesDataModule,
+)
+from pytorch_forecasting.data.data_module._base_datamodule_pkg import _BasePtDataModule
 from pytorch_forecasting.data.data_module._encoder_decoder_data_module import (
     EncoderDecoderTimeSeriesDataModule,
 )
+from pytorch_forecasting.data.data_module._encoder_decoder_datamodule_pkg import (
+    EncoderDecoderDataModule_pkg,
+)
 from pytorch_forecasting.data.data_module._tslib_data_module import TslibDataModule
+from pytorch_forecasting.data.data_module._tslib_datamodule_pkg import (
+    TslibDataModule_pkg,
+)
 
-__all__ = ["EncoderDecoderTimeSeriesDataModule", "TslibDataModule"]
+__all__ = [
+    "BaseTimeSeriesDataModule",
+    "EncoderDecoderTimeSeriesDataModule",
+    "TslibDataModule",
+    "_BasePtDataModule",
+    "EncoderDecoderDataModule_pkg",
+    "TslibDataModule_pkg",
+]
