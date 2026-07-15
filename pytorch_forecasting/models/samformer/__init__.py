b26859b9e44d039b451a048632cb58640ec8bcdb
yusuke8h <yusuke8h@gmail.com>
2026-07-16 00:07:02 +0900

    fix: [ENH] Rename `*_v2_pkg.py` to `*_pkg_v2.py` (#2339)

--- pytorch_forecasting/models/samformer/__init__.py
+++ pytorch_forecasting/models/samformer/__init__.py
@@ -4,6 +4,6 @@ DSIPTS Implementation of Samformer for V2
 """
 
 from pytorch_forecasting.models.samformer._samformer_v2 import Samformer
-from pytorch_forecasting.models.samformer._samformer_v2_pkg import Samformer_pkg_v2
+from pytorch_forecasting.models.samformer._samformer_pkg_v2 import Samformer_pkg_v2
 
 __all__ = ["Samformer", "Samformer_pkg_v2"]
