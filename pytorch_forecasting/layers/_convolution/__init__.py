29b3e92cc485058e83227e5b5c04801b3089171f
Muhammad-Rebaal <mrebaal14@gmail.com>
2026-08-12 03:18:29 +0500

    Fix: Code Quality

--- pytorch_forecasting/layers/_convolution/__init__.py
+++ pytorch_forecasting/layers/_convolution/__init__.py
@@ -1,3 +1,5 @@
-from pytorch_forecasting.layers._convolution._reparam_large_kernel_conv import ReparamLargeKernelConv
+from pytorch_forecasting.layers._convolution._reparam_large_kernel_conv import (
+    ReparamLargeKernelConv,
+)
 
 __all__ = ["ReparamLargeKernelConv"]
