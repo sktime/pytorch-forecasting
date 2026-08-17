aefbf45531d747c5bc32b95e088cf07062b4a782
Echo Xiao <echoxiao666@gmail.com>
2026-08-17 14:17:05 -0700

    [ENH] move SCIBlock to layers/_blocks following v2 layer layout

--- pytorch_forecasting/layers/_blocks/__init__.py
+++ pytorch_forecasting/layers/_blocks/__init__.py
@@ -1,3 +1,7 @@
 from pytorch_forecasting.layers._blocks._residual_block_dsipts import ResidualBlock
+from pytorch_forecasting.layers._blocks._scinet_block import SCIBlock
 
-__all__ = ["ResidualBlock"]
+__all__ = [
+    "ResidualBlock",
+    "SCIBlock",
+]
